9d96cfba1bab98b6f8304b80cea01f4e1a9c509e
Piti (Fyi) Champeethong <276376+ninefyi@users.noreply.github.com>
2023-05-23 10:00:16 +0000

    init projects

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # Noted
+
+## Reference
+- [kkoziarski](https://dev.to/kkoziarski/react-vite-github-codespaces-5529)
---
+++ thbaankk/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SearchItem from './components/SearchItem.vue'
+</script>
+
+<template>
+  <div>
+    <SearchItem />
+  </div>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
