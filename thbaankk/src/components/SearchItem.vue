0f261cc17bdf81fec1d4d75e845781a817ef7752
Piti (Fyi) Champeethong <276376+ninefyi@users.noreply.github.com>
2023-06-20 05:05:25 +0000

    update component

--- thbaankk/src/components/SearchItem.vue
+++ thbaankk/src/components/SearchItem.vue
@@ -2,7 +2,8 @@
   <div>
     <i>
       <p>My search query is: {{ message }}</p>
-      <input v-model="message" placeholder="edit me" />
+      <input v-model="message" placeholder="enter your query" />&nbsp;
+      <button @click="fetchData">Query Please!</button>
     </i>
     <div class="details"></div>
   </div>
@@ -13,6 +14,25 @@ export default {
     return {
       message: '',
     }
+  },
+  methods: {
+    fetchData() {
+      fetch('https://ap-southeast-1.aws.data.mongodb-api.com/app/singer-jllwn/endpoint/search', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "query": this.message })
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            console.log(data);
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
   },
 }
 </script>
